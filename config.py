70484c4010900ede3b08974882d8c34c11c9c784
Paket (Пакет) <91895055+userMurder@users.noreply.github.com>
2024-08-17 20:33:10 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+#Тут ваши токены, от кб и от бота
+API_TOKEN = ""
+CRYPTOPAY_API_TOKEN = ""
+
+
+#Cюда тока 1 chatid вписываем
+admin_id = 
+
+
+
+#Коофицент на победу т.е 0.8 это по факту 1.8 а 1 это 2
+coefficient = 0.8 # Коф на кубик
+box_cof = 1
+
+
+# Файл базы данных для логов не трогайте
+LOG_FILE = 'casino_log.db'  
+REFERRAL_FILE = "reffelal.db"
+
+#ID Канала/Чата куда пересылаються выигрышы
+win_id = 
+#Админские логи
+chat_id_log = 
+
+#Название казино
+BOT_USERNAME = '' 
+
+
+
+
+
